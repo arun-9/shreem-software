1e8806e093007d15a522427b714c55160b0b33eb
manojLovesCoding <mkum9417@gmail.com>
2025-02-06 10:33:19 +0530

    navbar-segment

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,11 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
-import Homepage from "./components/Homepage";
-import About from "./components/pages/About";
-//import Services from "./components/pages/Services";
-import Contact from "./components/pages/Contact";
+import Navbar from "./segments/Navbar/Navbar";
 
-function App() {
+const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/about" element={<About />} />
-        {/*<Route path="/services" element={<Services />} />*/}
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Navbar />
+    </>
   );
-}
+};
 
 export default App;
