880f08c1fe41566db573ccadfdbc41dbcac091a2
manojLovesCoding <mkum9417@gmail.com>
2025-02-07 00:05:30 +0530

    hero section - animation

--- src/segments/Hero/Hero.jsx
+++ src/segments/Hero/Hero.jsx
@@ -5,7 +5,19 @@ import { FaArrowTrendUp } from "react-icons/fa6";
 import { FaProjectDiagram } from "react-icons/fa";
 import HeroImage from "../../assets/hero.png";
 import BoxAnimation from "../../assets/BoxAnimation";
+import gsap from "gsap";
+import { useEffect } from "react";
 const Hero = () => {
+  useEffect(() => {
+    gsap
+      .timeline({ delay: 0.5 })
+      .fromTo("#hero .hero-subtitle", { x: -20, opacity: 0 }, { x: 0, opacity: 1 })
+      .fromTo("#hero .hero-title", { y: 20, opacity: 0 }, { y: 0, opacity: 1 })
+      .fromTo("#hero .hero-image", { y: -20, opacity: 0 }, { y: 0, opacity: 11 })
+      .fromTo("#hero .hero-description", { y: 20, opacity: 0 }, { y: 0, opacity: 1 })
+      .fromTo("#hero .button-wrapper", { y: 20, opacity: 0 }, { y: 0, opacity: 1 });
+  });
+
   return (
     <section id="hero">
       <BoxAnimation />
@@ -17,16 +29,12 @@ const Hero = () => {
           >
             Innovating Tomorrow, Today
           </h4>
-          <h1 className="hero-title">
-            Shree Software – Software You Can Trust
-          </h1>
+          <h1 className="hero-title">Shree Software – Software You Can Trust</h1>
           <p className="hero-description">
-            Shreem Software Pvt. Ltd. is committed to delivering smart,
-            customized, and reliable software solutions. We collaborate with
-            businesses to turn challenges into opportunities through technology.
-            Whether it is web development, cloud solutions, or enterprise
-            software, we ensure quality, efficiency, and customer satisfaction
-            at every step.
+            Shreem Software Pvt. Ltd. is committed to delivering smart, customized, and reliable
+            software solutions. We collaborate with businesses to turn challenges into opportunities
+            through technology. Whether it is web development, cloud solutions, or enterprise
+            software, we ensure quality, efficiency, and customer satisfaction at every step.
           </p>
           <div className="button-wrapper">
             <Link to="contact" smooth={true} className="btn primary">
