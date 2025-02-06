32019346721d1519d70db547b4625e112cde4125
manojLovesCoding <mkum9417@gmail.com>
2025-02-06 23:49:56 +0530

    navbar-animation animated : company-logo,socials-logo and nav-links

--- src/segments/Navbar/Navbar.jsx
+++ src/segments/Navbar/Navbar.jsx
@@ -5,8 +5,9 @@ import { Link } from "react-scroll";
 import Socials from "../../components/Socials/Socials";
 import { CgMenuRight } from "react-icons/cg";
 import Sidebar from "../../components/Sidebar/Sidebar";
-import { useMemo, useState } from "react";
+import { useMemo, useState, useEffect } from "react";
 import { convertHexToRgba } from "../../util";
+import gsap from "gsap";
 const Navbar = () => {
   const [open, setOpen] = useState(false);
   const [drop, setDrop] = useState(false);
@@ -23,12 +24,22 @@ const Navbar = () => {
     return () => window.removeEventListener("scroll", dropNavbar);
   });
 
+  useEffect(() => {
+    gsap
+      .timeline({ delay: 0.5 })
+      .fromTo("#navbar .logo", { x: -50, opacity: 0 }, { x: 0, opacity: 1 })
+      .fromTo("#navbar .socials", { x: 50, opacity: 0 }, { x: 0, opacity: 1 })
+      .fromTo(
+        "#navbar .route-wrapper .route",
+        { x: -50, opacity: 0 },
+        { x: 0, opacity: 1, stagger: 0.2 },
+      );
+  });
+
   return (
     <>
       <Sidebar open={open} onClose={() => setOpen(!open)} />
-      {open && (
-        <div className="sidebar-overlay" onClick={() => setOpen(!open)} />
-      )}
+      {open && <div className="sidebar-overlay" onClick={() => setOpen(!open)} />}
       <nav
         id="navbar"
         className={drop ? "blur drop" : ""}
